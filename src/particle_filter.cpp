3b67a44d92d807692318f70df079dae46d627245
arunchavan89 <arunchavan89@gmail.com>
2020-04-21 11:54:28 +0200

    Correcting prediction function in particle_filter.cpp

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -30,16 +30,23 @@ void ParticleFilter::init(double x, double y, double theta, double std[])
      * Add random Gaussian noise to each particle.
      *
      */
-    num_particles = 1000;  // Set the number of particles
+
+    num_particles = 10;  // Set the number of particles
+
+    /* Add noise to each measurement */
+    std::default_random_engine gen;
+    std::normal_distribution<double> dist_x(x, std[0]);
+    std::normal_distribution<double> dist_y(y, std[1]);
+    std::normal_distribution<double> dist_theta(theta, std[2]);
 
     for (int i = 0; i < num_particles; ++i)
     {
         /*Initialize all 1000 particles to first position*/
         Particle temp_particle;
         temp_particle.id = i;
-        temp_particle.x = x + std[0];
-        temp_particle.y = y + std[1];
-        temp_particle.theta = theta + std[2];
+        temp_particle.x = dist_x(gen);
+        temp_particle.y = dist_y(gen);
+        temp_particle.theta = dist_theta(gen);
         temp_particle.weight = 1.0;
 
         particles.push_back(temp_particle);
@@ -62,34 +69,39 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 
     std::random_device rd{};
     std::mt19937 gen{ rd() };
-    std::normal_distribution<float>d1(0, std_pos[0]);
-    std::normal_distribution<float>d2(0, std_pos[1]);
-    std::normal_distribution<float>d3(0, std_pos[2]);
-
-    float temp_noise_x = std::round(d1(gen));
-    float temp_noise_y = std::round(d2(gen));
-    float temp_noise_theta = std::round(d3(gen));
 
     for (int i = 0; i < particles.size(); ++i)
     {
+        Particle particle = particles[i];
+
         if (yaw_rate > 0.0001) /* Avoid division by zero. */
         {
             /* Update distances: https://github.com/arunchavan89/CarND-Kidnapped-Vehicle-Project/blob/master/formulae.pdf */
-            particles[i].x = particles[i].x + (velocity / yaw_rate)  * (sin(particles[i].theta + (yaw_rate * delta_t)) - sin(particles[i].theta)) + temp_noise_x;
-            particles[i].y = particles[i].y + (velocity / yaw_rate)  * (cos(particles[i].theta) - cos(particles[i].theta + (yaw_rate * delta_t))) + temp_noise_y;
+            particle.x = particle.x + (velocity / yaw_rate)  * (sin(particle.theta + yaw_rate * delta_t) - sin(particle.theta));
+            particle.y = particle.y + (velocity / yaw_rate)  * (cos(particle.theta) - cos(particle.theta + yaw_rate * delta_t));
 
             /* Update orientation */
-            particles[i].theta = particles[i].theta + yaw_rate * delta_t + temp_noise_theta;
+            particle.theta = particle.theta + yaw_rate * delta_t;
         }
         else
         {
             /* Update distances */
-            particles[i].x = particles[i].x + cos(yaw_rate) * (velocity * delta_t) + temp_noise_x;
-            particles[i].y = particles[i].y + sin(yaw_rate) * (velocity * delta_t) + temp_noise_y;
+            particle.x = particle.x + cos(yaw_rate) * (velocity * delta_t);
+            particle.y = particle.y + sin(yaw_rate) * (velocity * delta_t);
 
             /* Update orientation */
-            particles[i].theta = particles[i].theta + temp_noise_theta;
+            particle.theta = particle.theta;
         }
+
+        std::normal_distribution<double>x(particle.x, std_pos[0]);
+        std::normal_distribution<double>y(particle.y, std_pos[1]);
+        std::normal_distribution<double>theta(particle.theta, std_pos[2]);
+
+        particle.x = x(gen);
+        particle.y = y(gen);
+        particle.theta = theta(gen);
+
+        particles[i] = particle;
     }
 
 }
@@ -106,6 +118,14 @@ void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted, vector<Landm
      */
 
 }
+
+double calculateLikelihood(double x, double ux, double y, double uy, double sig_x, double sig_y) {
+    double gauss_norm = 1 / (2 * M_PI * sig_x * sig_y);
+    double exponent = ((x - ux) * (x - ux) / (2.0 * sig_x * sig_x)) + ((y - uy) * (y - uy) / (2.0 * sig_y * sig_y));
+    double likelihood = gauss_norm * exp(-exponent);
+    return likelihood;
+}
+
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
     const vector<LandmarkObs> &observations,
@@ -125,30 +145,43 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
      *   (look at equation 3.33) http://planning.cs.uiuc.edu/node99.html
      */
 
-    for (int i = 0; i < particles.size(); ++i)
-    {
-        Particle particle = particles[i];
-        double prob = 1.0;
+    for (int i = 0; i < num_particles; i++) {
+        Particle &p = particles[i];
+        double prob = 1;
+        for (int j = 0; j < observations.size(); j++) {
 
-        for (int j = 0; j < observations.size(); j++)
-        {
-            /* Homogenous Transformation https://github.com/arunchavan89/CarND-Kidnapped-Vehicle-Project/blob/master/formulae.pdf */
-            double trans_x = particle.x + (cos(particle.theta) * observations[j].x) - (sin(particle.theta) * observations[j].y);
-            double trans_y = particle.y + (sin(particle.theta) * observations[j].x) + (cos(particle.theta) * observations[j].y);
+            double obj_x = observations[j].x;
+            double obj_y = observations[j].y;
 
-            for (int k = 0; k < map_landmarks.landmark_list.size(); k++)
-            {
-                /* Calculate distance between particle and landmarks */
-                double distance = dist(trans_x, trans_y, map_landmarks.landmark_list[k].x_f, map_landmarks.landmark_list[k].y_f);
-                if (distance <= sensor_range)
-                {
-                    prob *= calculate_multvariate_normal_distribution(std_landmark[0], std_landmark[1], trans_x, trans_y, map_landmarks.landmark_list[k].x_f, map_landmarks.landmark_list[k].y_f);
+            // Transformation from particle coordinate to Map coordinate system
+            double trans_x = p.x + cos(p.theta) * obj_x - sin(p.theta) * obj_y;
+            double trans_y = p.y + sin(p.theta) * obj_x + cos(p.theta) * obj_y;
+
+            vector<Map::single_landmark_s> landmarks = map_landmarks.landmark_list;
+            double min_dist = 1000000;
+            double land_x = -1;
+            double land_y = -1;
+
+            for (int k = 0; k < landmarks.size(); k++) {
+                double act_x = landmarks[k].x_f;
+                double act_y = landmarks[k].y_f;
+                double dist = sqrt((act_x - trans_x) * (act_x - trans_x) + (act_y - trans_y) * (act_y - trans_y));
+
+                if (dist < min_dist && dist <= sensor_range) {
+                    min_dist = dist;
+                    land_x = act_x;
+                    land_y = act_y;
                 }
             }
 
+            // calculate likelihood using 2D - gaussian distribution
+            // https://stackoverflow.com/questions/41538095/evaluate-multivariate-normal-gaussian-density-in-c
+            double likelihood = calculateLikelihood(land_x, trans_x, land_y, trans_y, std_landmark[0], std_landmark[1]);
+            prob *= likelihood;
         }
-        particles[i].weight = prob;
+        p.weight = prob;
         weights[i] = prob;
+
     }
 }
 
@@ -162,17 +195,14 @@ void ParticleFilter::resample()
      */
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::discrete_distribution<> discrete_weights(weights.begin(), weights.end());
+    std::discrete_distribution<> d(weights.begin(), weights.end());
+    std::vector<Particle> resampled_particles;
 
-    std::vector<Particle>resampled_weights;
-    for (int i = 0; i < num_particles; i++)
-    {
-        Particle particle;
-        particle = particles[discrete_weights(gen)];
-        resampled_weights.push_back(particle);
+    for (int n = 0; n < num_particles; ++n) {
+        Particle particle = particles[d(gen)];
+        resampled_particles.push_back(particle);
     }
-
-    particles = resampled_weights;
+    particles = resampled_particles;
 }
 
 void ParticleFilter::SetAssociations(Particle& particle,
